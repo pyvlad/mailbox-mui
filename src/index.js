e11b084e1840452d95965bad0cbb7828791821e1
PyVlad <32043790+pyvlad@users.noreply.github.com>
2019-08-05 16:15:10 +0300

    set up: install material-ui (with roboto fonts, font icons, and SVG icons)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// add @font-face rules in CSS:
+import 'typeface-roboto';
+// Typography injects CSS classes for each variant
+// (with Roboto as first font-family):
+import Typography from '@material-ui/core/Typography';
+// font-icons are installed locally directly in the 'public' folder
+import Icon from '@material-ui/core/Icon';
+import { AccessAlarm } from '@material-ui/icons';
+
+
 ReactDOM.render(
-  <h1>MUI Mailbox</h1>, 
+  <div>
+    <Typography variant="h1">
+      MUI Mailbox
+    </Typography>
+    <Icon>star</Icon>
+    <AccessAlarm />
+  </div>,
   document.getElementById('root')
 );
